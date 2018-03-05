3e14ecea12fedb752454b794c8b0d1c9a16644bb
Alice <hanyuqin2012@163.com>
2018-03-05 13:24:37 +0800

    Alice 2018-3-5 价格生效时间

---
+++ acerchem/acerchemcore/src/com/acerchem/core/suggestion/PriceRowSearchService.java
@@ -0,0 +1,10 @@
+package com.acerchem.core.suggestion;
+
+import de.hybris.platform.europe1.model.PriceRowModel;
+/**
+ * Alice
+ */
+public interface PriceRowSearchService
+{
+	void getReferencesForProductInPriceRow(PriceRowModel priceRow);
+}
