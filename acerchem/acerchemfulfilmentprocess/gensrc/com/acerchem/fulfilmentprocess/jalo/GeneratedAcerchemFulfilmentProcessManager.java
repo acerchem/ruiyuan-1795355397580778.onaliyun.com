008237a6db14f4bfafc193fd0ef61b25d2658dfa
wangjiehua <wangjiehua@qq.com>
2018-03-18 01:55:32 +0800

    整理

--- acerchem/acerchemfulfilmentprocess/gensrc/com/acerchem/fulfilmentprocess/jalo/GeneratedAcerchemFulfilmentProcessManager.java
+++ acerchem/acerchemfulfilmentprocess/gensrc/com/acerchem/fulfilmentprocess/jalo/GeneratedAcerchemFulfilmentProcessManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 2018-3-18 1:19:43                           ---
+ * --- Generated at 2018-3-18 1:53:06                           ---
  * ----------------------------------------------------------------
  */
 package com.acerchem.fulfilmentprocess.jalo;
