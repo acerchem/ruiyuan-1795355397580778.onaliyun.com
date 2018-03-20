2a8cbcf0f72e434392e842c1f75dd74fbd5e850d
unknown <18014351085@163.com>
2018-03-20 14:44:29 +0800

    add addressDataList for customerData

---
+++ acerchem/acerchemfacades/src/com/acerchem/facades/facades/AcerchemCustomerFacade.java
@@ -0,0 +1,11 @@
+package com.acerchem.facades.facades;
+
+import de.hybris.platform.commercefacades.user.data.CustomerData;
+
+/**
+ * Created by Jacob.Ji on 2018/3/20.
+ */
+
+public interface AcerchemCustomerFacade {
+    CustomerData getCurrentCustomer();
+}
