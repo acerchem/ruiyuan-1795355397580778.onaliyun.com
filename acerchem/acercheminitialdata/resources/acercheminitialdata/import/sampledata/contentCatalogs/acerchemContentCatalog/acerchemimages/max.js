2334d251e5b2a59578d74a55cbbf98f107d5d2f1
shaun.sheng <shaun.sheng@ibreakingpoint.com>
2018-03-31 20:17:26 +0800

    主数据

---
+++ acerchem/acercheminitialdata/resources/acercheminitialdata/import/sampledata/contentCatalogs/acerchemContentCatalog/acerchemimages/max.js
@@ -0,0 +1,40 @@
+//rem单位换算
+	var currClientWidth, fontValue,originWidth;
+    originWidth=375;
+    __resize();
+
+    window.addEventListener('resize', __resize, false);
+    function __resize() {
+        currClientWidth = document.documentElement.clientWidth;
+        if (currClientWidth > 640) currClientWidth = 640;
+        if (currClientWidth < 320) currClientWidth = 320;
+        fontValue = ((62.5 * currClientWidth) /originWidth).toFixed(2);
+        document.documentElement.style.fontSize = fontValue + '%';
+    }
+    
+
+//gotop
+$('.gotop').on('click',function(){
+  $("body").animate({scrollTop:0},300);
+})
+
+// top srarch
+
+$(document).on({'click':function(){ $('.searchlist').hide();},'scroll':function(){ $('.searchlist').hide();}})
+
+$('#search,.maxtop_rig .maxicon-search').on('click',function(e){
+  e.preventDefault();
+  $('.searchlist .lab-row').animate({'width':'100%'})
+  $('.searchlist').show();
+  return false;
+})
+
+$('#search .lab-row').on('focus',function(e){
+  $('.pdlist').show();
+  return false;
+})
+
+$('#search .btn-submit').on('click',function(){
+  $('#search').submit();
+})
+
