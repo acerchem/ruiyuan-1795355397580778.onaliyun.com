cb765b65262c5c248132358b4c7948a860ba91ed
Alice <alice.han@ibreakingpoint.com>
2018-04-04 05:48:43 -0400

    Update AcerchemCustomerPopulator.java

--- acerchem/acerchemfacades/src/com/acerchem/facades/populators/AcerchemCustomerPopulator.java
+++ acerchem/acerchemfacades/src/com/acerchem/facades/populators/AcerchemCustomerPopulator.java
@@ -1,8 +1,8 @@
 package com.acerchem.facades.populators;
 
-import de.hybris.platform.commercefacades.user.converters.populator.AddressPopulator;
 import de.hybris.platform.commercefacades.user.converters.populator.CustomerPopulator;
 import de.hybris.platform.commercefacades.user.data.AddressData;
+import de.hybris.platform.commercefacades.user.data.CustomerCreditAccountData;
 import de.hybris.platform.commercefacades.user.data.CustomerData;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.core.model.user.AddressModel;
@@ -11,22 +11,33 @@ import de.hybris.platform.servicelayer.dto.converter.ConversionException;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
 import org.springframework.beans.factory.annotation.Required;
 
+import com.acerchem.core.model.CustomerCreditAccountModel;
+
 import java.util.Objects;
 
 public class AcerchemCustomerPopulator extends CustomerPopulator implements Populator<CustomerModel, CustomerData>
 {
 	private Converter<AddressModel,AddressData> addressConverter;
-
+	private Converter<CustomerCreditAccountModel,CustomerCreditAccountData> creditAccount;
+	
 	@Override
 	public void populate(CustomerModel source, CustomerData target) throws ConversionException {
 		super.populate(source,target);
 		if (Objects.nonNull(source.getAddresses())) {
 			target.setAddressDataList(addressConverter.convertAll(source.getAddresses()));
 		}
+		if (source.getCreditAccount() != null)
+		{
+			target.setCreditAccount(creditAccount.convert(source.getCreditAccount()));
+		}
+		
 	}
 
 	@Required
 	public void setAddressConverter(Converter<AddressModel, AddressData> addressConverter) {
 		this.addressConverter = addressConverter;
 	}
+	
+	
 }
+
