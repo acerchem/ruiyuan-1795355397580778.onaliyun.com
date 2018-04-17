e8c4d62e6181c89c9919c89b70f1581bc86e94ab
John Tao <john.tao@ibreakingpoint.com>
2018-04-17 17:03:04 +0800

    order-process 添加event事件

---
+++ acerchem/acerchemfulfilmentprocess/src/com/acerchem/fulfilmentprocess/actions/consignment/SplitDeliveryModeAction.java
@@ -0,0 +1,53 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.acerchem.fulfilmentprocess.actions.consignment;
+
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.orderprocessing.model.OrderProcessModel;
+import de.hybris.platform.processengine.action.AbstractSimpleDecisionAction;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * This example action checks the order for required data in the business process. Skipping this action may result in
+ * failure in one of the subsequent steps of the process. The relation between the order and the business process is
+ * defined in basecommerce extension through item OrderProcess. Therefore if your business process has to access the
+ * order (a typical case), it is recommended to use the OrderProcess as a parentClass instead of the plain
+ * BusinessProcess.
+ */
+public class SplitDeliveryModeAction extends AbstractSimpleDecisionAction<OrderProcessModel>
+{
+	private static final Logger LOG = Logger.getLogger(SplitDeliveryModeAction.class);
+
+
+	@Override
+	public Transition executeAction(final OrderProcessModel process)
+	{
+		final OrderModel order = process.getOrder();
+
+		if (order == null)
+		{
+			LOG.error("Missing the order, exiting the process");
+			return Transition.NOK;
+		}
+
+		if (order.getDeliveryMode().getCode().equals("здЬс"))
+		{
+			return Transition.OK;
+		}
+		else
+		{
+			return Transition.NOK;
+		}
+	}
+
+}
