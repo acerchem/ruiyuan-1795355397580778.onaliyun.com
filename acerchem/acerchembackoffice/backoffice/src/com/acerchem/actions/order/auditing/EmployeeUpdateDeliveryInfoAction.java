d34d410de9686253124020b9023f7afc66a37784
John Tao <john.tao@ibreakingpoint.com>
2018-04-20 18:12:23 +0800

    按钮添加

---
+++ acerchem/acerchembackoffice/backoffice/src/com/acerchem/actions/order/auditing/EmployeeUpdateDeliveryInfoAction.java
@@ -0,0 +1,58 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.acerchem.actions.order.auditing;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+
+import com.hybris.cockpitng.actions.ActionContext;
+import com.hybris.cockpitng.actions.ActionResult;
+import com.hybris.cockpitng.actions.CockpitAction;
+import com.hybris.cockpitng.engine.impl.AbstractComponentWidgetAdapterAware;
+
+import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.processengine.BusinessProcessEvent;
+import de.hybris.platform.processengine.BusinessProcessService;
+import de.hybris.platform.servicelayer.model.ModelService;
+
+public class EmployeeUpdateDeliveryInfoAction extends AbstractComponentWidgetAdapterAware implements CockpitAction<OrderModel, Object>
+{
+	private static final Logger LOG = Logger.getLogger(EmployeeUpdateDeliveryInfoAction.class);
+	private static final String CURRENT_OBJECT = "currentObject";
+	
+	@Resource
+	BusinessProcessService businessProcessService;
+	
+	@Resource
+	private ModelService modelService;
+	
+	public BusinessProcessService getBusinessProcessService() {
+		return businessProcessService;
+	}
+
+	public void setBusinessProcessService(BusinessProcessService businessProcessService) {
+		this.businessProcessService = businessProcessService;
+	}
+
+
+	@Override
+	public ActionResult<Object> perform(ActionContext<OrderModel> ctx) {
+		// TODO Auto-generated method stub
+		LOG.info("--------------------start EmployeeUpdateDeliveryInfoAction-------------------");
+		OrderModel order = (OrderModel) ctx.getData();
+		final String processCode = order.getOrderProcess().iterator().next().getCode();
+		getBusinessProcessService().triggerEvent(processCode + "_"+ "WaitConfrimConsignmentForEmployee");
+		LOG.info("--------------------end EmployeeUpdateDeliveryInfoAction-------------------"+processCode);
+		return new ActionResult("success");
+	}
+	
+}
