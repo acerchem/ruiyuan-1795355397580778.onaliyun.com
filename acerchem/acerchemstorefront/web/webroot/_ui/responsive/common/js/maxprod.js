3429bf576cd85cfd9fd16c0d363553f3eb6d7a8c
shaun.sheng <shaun.sheng@ibreakingpoint.com>
2018-05-11 01:36:37 -0400

    Update maxprod.js

--- acerchem/acerchemstorefront/web/webroot/_ui/responsive/common/js/maxprod.js
+++ acerchem/acerchemstorefront/web/webroot/_ui/responsive/common/js/maxprod.js
@@ -286,8 +286,7 @@ $(document).ready(function(){
 
 $(document).on('click','#addToCartButton',function(){
 	setTimeout(function(){
-		var acbox = $('#colorbox');
-		acbox.css({width:'100%',height:'100%',left:0,top:0})
+		$('#cboxWrapper , #cboxContent , #cboxLoadedContent').css({width:'',height:'',float:''})
 		console.log('colorbox')
 	},300)
 })
