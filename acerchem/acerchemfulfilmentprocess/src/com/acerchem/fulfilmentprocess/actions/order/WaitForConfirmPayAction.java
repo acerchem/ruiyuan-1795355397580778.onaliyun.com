3ad9ab4a1491405dabaf1880525bf58aeb71165f
John Tao <john.tao@ibreakingpoint.com>
2018-05-11 17:49:15 +0800

    订单状态变更

--- acerchem/acerchemfulfilmentprocess/src/com/acerchem/fulfilmentprocess/actions/order/WaitForConfirmPayAction.java
+++ acerchem/acerchemfulfilmentprocess/src/com/acerchem/fulfilmentprocess/actions/order/WaitForConfirmPayAction.java
@@ -45,6 +45,7 @@ public class WaitForConfirmPayAction extends AbstractSimpleDecisionAction<OrderP
 
 		if (order.getEmployeeConfirmPay() && order.getCustomerConfirmPay())
 		{
+			setOrderStatus(order, OrderStatus.PROCESSING);
 			//setOrderStatus(order, OrderStatus.PAIED);
 			return Transition.OK;
 		}
