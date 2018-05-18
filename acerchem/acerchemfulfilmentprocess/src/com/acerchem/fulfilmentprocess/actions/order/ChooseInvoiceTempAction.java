7a19f324b5139c86a06788ec3efce47070dcd41d
Alice <alice.han@ibreakingpoint.com>
2018-05-18 01:35:33 -0400

    Update ChooseInvoiceTempAction.java

--- acerchem/acerchemfulfilmentprocess/src/com/acerchem/fulfilmentprocess/actions/order/ChooseInvoiceTempAction.java
+++ acerchem/acerchemfulfilmentprocess/src/com/acerchem/fulfilmentprocess/actions/order/ChooseInvoiceTempAction.java
@@ -11,14 +11,12 @@
 package com.acerchem.fulfilmentprocess.actions.order;
 
 import de.hybris.platform.core.model.order.OrderModel;
+import de.hybris.platform.core.model.user.AddressModel;
 import de.hybris.platform.orderprocessing.model.OrderProcessModel;
 import de.hybris.platform.processengine.action.AbstractAction;
-import de.hybris.platform.processengine.action.AbstractSimpleDecisionAction.Transition;
 import de.hybris.platform.task.RetryLaterException;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import org.apache.log4j.Logger;
 
 
@@ -60,30 +58,35 @@ public class ChooseInvoiceTempAction extends AbstractAction<OrderProcessModel>
 	protected Transition executeAction(final OrderProcessModel process)
 	{
 		final OrderModel order = process.getOrder();
-		/*LOG.info("-------------------------------------"+order.getEntries().get(0).getDeliveryPointOfService().getDeliveryZone().getCode());
-		if("GB".equals(order.getEntries().get(0).getDeliveryPointOfService().getDeliveryZone().getCode())){
-			if("DCM".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode()) || "Neele-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode()) || "UK".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
+		LOG.info("-------------------------------------"+order.getEntries().get(0).getDeliveryPointOfService().getDeliveryZone().getCode());
+		
+		AddressModel addressModel = null;
+		if(order.getDeliveryMode().getCode().equals("DELIVERY_MENTION"))
+		{//自提
+			for(AddressModel address:order.getUser().getAddresses())
+			{
+				if(address.getContactAddress())
+				{
+					addressModel=address;
+				}
+			}
+		}
+		else
+		{//送货
+			addressModel=order.getDeliveryAddress();
+		}
+		
+		if("Neele-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
+			return Transition.TEMP4;
+		}else if("UK-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
+			if("GB".equals(addressModel.getCountry().getIsocode())){
 				return Transition.TEMP2;
 			}
-		}else if("PL".equals(order.getEntries().get(0).getDeliveryPointOfService().getDeliveryZone().getCode())){
-			if("DCM".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
-				return Transition.TEMP5;
-			}else if("Neele-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
-				return Transition.TEMP1;
-			}else if("UK-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
-				return Transition.TEMP6;
-			}
-			
-		}else{
-			if("DCM".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
-				return Transition.TEMP3;
-			}else if("Neele-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
-				return Transition.TEMP4;
-			}else if("UK-vat".equals(order.getEntries().get(0).getDeliveryPointOfService().getWarehouses().get(0).getCode())){
+			else
+			{
 				return Transition.TEMP3;
 			}
 		}
-		return Transition.NOK; */
-		return Transition.TEMP1;
+		return Transition.NOK; 
 	}
 }
