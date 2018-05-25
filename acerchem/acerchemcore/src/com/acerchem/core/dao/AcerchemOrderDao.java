1a0de7d3e180a142a698952d597f57aba638c83c
Alice <alice.han@ibreakingpoint.com>
2018-05-25 04:03:59 -0400

    Add new file

---
+++ acerchem/acerchemcore/src/com/acerchem/core/dao/AcerchemOrderDao.java
@@ -0,0 +1,12 @@
+package com.acerchem.core.dao;
+
+import java.util.List;
+
+import de.hybris.platform.commercefacades.order.data.MonthlySalesAnalysis;
+import de.hybris.platform.commercefacades.order.data.OrderDetailsReportData;
+
+public interface AcerchemOrderDao {
+	
+	List<OrderDetailsReportData> getOrderDetails(Integer month,String area,String countryCode,String userName,String orderCode);
+	List<MonthlySalesAnalysis> getMonthlySalesAnalysis(Integer year,String area);
+}
