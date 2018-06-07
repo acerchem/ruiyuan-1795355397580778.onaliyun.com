77fee526e66bfd19ffdc3b60ce0192c61f54a639
Alice <alice.han@ibreakingpoint.com>
2018-06-07 02:48:12 -0400

    Update AcerchemOrderDao.java

--- acerchem/acerchemcore/src/com/acerchem/core/dao/AcerchemOrderDao.java
+++ acerchem/acerchemcore/src/com/acerchem/core/dao/AcerchemOrderDao.java
@@ -9,6 +9,6 @@ import de.hybris.platform.commercefacades.order.data.OrderDetailsReportData;
 public interface AcerchemOrderDao {
 	
 	Set<String> getAllAreas();
-	List<OrderDetailsReportData> getOrderDetails(Integer month,String area,String countryCode,String userName,String orderCode,Integer pageNumber);
+	List<OrderDetailsReportData> getOrderDetails(String month,String area,String countryCode,String userName,String orderCode,Integer pageNumber);
 	List<MonthlySalesAnalysis> getMonthlySalesAnalysis(Integer year,String area);
 }
