3ab844a7f0e8a710c819c89c8262a53f25043dc6
wangjiehua <wangjiehua@qq.com>
2018-08-22 11:02:07 +0800

    优化

--- acerchem/acerchemstorefront/web/src/com/acerchem/storefront/controllers/reports/ReportHomePageController.java
+++ acerchem/acerchemstorefront/web/src/com/acerchem/storefront/controllers/reports/ReportHomePageController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractPageController;
+import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
+import de.hybris.platform.cms2.model.pages.AbstractPageModel;
 
 /**
  * Home of report
@@ -17,8 +19,15 @@ import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.Abstrac
 public class ReportHomePageController extends AbstractPageController{
 
 	@RequestMapping(value={"report","report/"},method = RequestMethod.GET)
-	public String homeReport(final Model model){
+	public String homeReport(final Model model) throws CMSItemNotFoundException{
+		storeCmsPageInModel(model, getContentPageForLabelOrId(null));
+		setUpMetaDataForContentPage(model, getContentPageForLabelOrId(null));
+		updatePageTitle(model, getContentPageForLabelOrId(null));
 		
 		return REDIRECT_PREFIX + "/reports/productPriceAnalysis";
+	}
+	
+	protected void updatePageTitle(final Model model, final AbstractPageModel cmsPage) {
+		storeContentPageTitleInModel(model, getPageTitleResolver().resolveHomePageTitle(cmsPage.getTitle()));
 	}
 }
