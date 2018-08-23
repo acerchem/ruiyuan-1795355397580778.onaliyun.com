5b1f39d41b015db5535cf245705b9b44b26efbd6
Alice <alice.han@ibreakingpoint.com>
2018-08-23 10:03:33 -0400

    Update CategoryPageController.java

--- acerchem/acerchemstorefront/web/src/com/acerchem/storefront/controllers/pages/CategoryPageController.java
+++ acerchem/acerchemstorefront/web/src/com/acerchem/storefront/controllers/pages/CategoryPageController.java
@@ -11,16 +11,32 @@
 package com.acerchem.storefront.controllers.pages;
 
 
+import de.hybris.platform.acceleratorservices.controllers.page.PageType;
+import de.hybris.platform.acceleratorservices.data.RequestContextData;
+import de.hybris.platform.acceleratorstorefrontcommons.constants.WebConstants;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.ThirdPartyConstants;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractCategoryPageController;
+import de.hybris.platform.acceleratorstorefrontcommons.util.MetaSanitizerUtil;
+import de.hybris.platform.category.model.CategoryModel;
+import de.hybris.platform.cms2.model.pages.CategoryPageModel;
+import de.hybris.platform.commercefacades.product.data.CategoryData;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.commercefacades.search.data.SearchStateData;
 import de.hybris.platform.commerceservices.search.facetdata.FacetRefinement;
-
+import de.hybris.platform.commerceservices.search.facetdata.ProductCategorySearchPageData;
+import de.hybris.platform.converters.Converters;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +52,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/**/c")
 public class CategoryPageController extends AbstractCategoryPageController {
-
+	
+	@Resource
+	private Converter<ProductModel, ProductData> productConverter;
+	
     @RequestMapping(value = CATEGORY_CODE_PATH_VARIABLE_PATTERN, method = RequestMethod.GET)
     public String category(@PathVariable("categoryCode") final String categoryCode, // NOSONAR
                            @RequestParam(value = "q", required = false) final String searchQuery,
@@ -44,7 +63,77 @@ public class CategoryPageController extends AbstractCategoryPageController {
                            @RequestParam(value = "show", defaultValue = "Page") final ShowMode showMode,
                            @RequestParam(value = "sort", required = false) final String sortCode, final Model model,
                            final HttpServletRequest request, final HttpServletResponse response) throws UnsupportedEncodingException {
-        return performSearchAndGetResultsPage(categoryCode, searchQuery, page, showMode, sortCode, model, request, response);
+    	
+    	//return performSearchAndGetResultsPage(categoryCode, searchQuery, page, showMode, sortCode, model, request, response);
+		
+    	final CategoryModel category = getCommerceCategoryService().getCategoryForCode(categoryCode);
+    	List<ProductData> products=new ArrayList<>();
+    	products.addAll(Converters.convertAll(category.getProducts(), productConverter));
+    	if(category.getAllSubcategories()!=null)
+    	{
+    		for(CategoryModel Subcategory:category.getAllSubcategories())
+        	{
+        		products.addAll(Converters.convertAll(Subcategory.getProducts(), productConverter));
+        	}
+    	}
+    	
+		final String redirection = checkRequestUrl(request, response, getCategoryModelUrlResolver().resolve(category));
+		if (StringUtils.isNotEmpty(redirection))
+		{
+			return redirection;
+		}
+
+		final CategoryPageModel categoryPage = getCategoryPage(category);
+
+		final CategorySearchEvaluator categorySearch = new CategorySearchEvaluator(categoryCode, searchQuery, page, showMode,
+				sortCode, categoryPage);
+
+		ProductCategorySearchPageData<SearchStateData, ProductData, CategoryData> searchPageData = null;
+		try
+		{
+			categorySearch.doSearch();
+			searchPageData = categorySearch.getSearchPageData();
+		}
+		catch (final ConversionException e) // NOSONAR
+		{
+			searchPageData = createEmptySearchResult(categoryCode);
+		}
+		catch (final Exception e) // NOSONAR
+		{
+			searchPageData = new ProductCategorySearchPageData<>();
+    		searchPageData.setResults(products);
+			searchPageData.setPagination(createEmptyPagination());
+			searchPageData.setCategoryCode(categoryCode);
+		}
+
+		final boolean showCategoriesOnly = categorySearch.isShowCategoriesOnly();
+
+		storeCmsPageInModel(model, categorySearch.getCategoryPage());
+		storeContinueUrl(request);
+
+		populateModel(model, searchPageData, showMode);
+		model.addAttribute(WebConstants.BREADCRUMBS_KEY, getSearchBreadcrumbBuilder().getBreadcrumbs(categoryCode, searchPageData));
+		model.addAttribute("showCategoriesOnly", Boolean.valueOf(showCategoriesOnly));
+		model.addAttribute("categoryName", category.getName());
+		model.addAttribute("pageType", PageType.CATEGORY.name());
+		model.addAttribute("userLocation", getCustomerLocationService().getUserLocation());
+
+		updatePageTitle(category, model);
+
+		final RequestContextData requestContextData = getRequestContextData(request);
+		requestContextData.setCategory(category);
+		requestContextData.setSearch(searchPageData);
+
+		if (searchQuery != null)
+		{
+			model.addAttribute(ThirdPartyConstants.SeoRobots.META_ROBOTS, ThirdPartyConstants.SeoRobots.NOINDEX_FOLLOW);
+		}
+
+		final String metaKeywords = MetaSanitizerUtil.sanitizeKeywords(category.getKeywords());
+		final String metaDescription = MetaSanitizerUtil.sanitizeDescription(category.getDescription());
+		setUpMetaData(model, metaKeywords, metaDescription);
+
+		return getViewPage(categorySearch.getCategoryPage());
     }
 
     @ResponseBody
