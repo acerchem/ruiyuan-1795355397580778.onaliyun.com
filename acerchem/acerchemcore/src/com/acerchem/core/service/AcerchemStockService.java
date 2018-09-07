a1218e444d2bf96b96a345a440082e7998e329fc
Alice <alice.han@ibreakingpoint.com>
2018-09-07 06:49:48 -0400

    alice 自定义下单验证库存功能

--- acerchem/acerchemcore/src/com/acerchem/core/service/AcerchemStockService.java
+++ acerchem/acerchemcore/src/com/acerchem/core/service/AcerchemStockService.java
@@ -1,5 +1,7 @@
 package com.acerchem.core.service;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import de.hybris.platform.core.model.order.AbstractOrderModel;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.ordersplitting.model.WarehouseModel;
@@ -11,4 +13,12 @@ public interface AcerchemStockService {
 	void calculateStock(ProductModel var1, WarehouseModel var2, int var3, String var4, boolean isUseFutureStock) throws InsufficientStockLevelException;
 
 	void releaseStock(AbstractOrderModel abstractOrderModel);
+	
+	/**
+	 * 下单时校验库存
+	 * @param redirectModel
+	 * @return
+	 */
+	boolean validateCart(RedirectAttributes redirectModel);
+	
 }
