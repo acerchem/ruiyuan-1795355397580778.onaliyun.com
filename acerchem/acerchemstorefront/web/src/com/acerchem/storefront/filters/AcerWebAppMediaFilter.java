9a0c36562e9d0d8dd4f6d972111b26de2f3e3bdc
ricky.li <281423889@qq.com>
2019-06-10 15:02:17 +0800

    ACDEV-188, 图片过滤x-oss-process

--- acerchem/acerchemstorefront/web/src/com/acerchem/storefront/filters/AcerWebAppMediaFilter.java
+++ acerchem/acerchemstorefront/web/src/com/acerchem/storefront/filters/AcerWebAppMediaFilter.java
@@ -1,5 +1,16 @@
 package com.acerchem.storefront.filters;
 
-public class WebAppMediaFilter
+import de.hybris.platform.servicelayer.web.WebAppMediaFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class AcerWebAppMediaFilter extends WebAppMediaFilter
 {
+	@Override
+	protected Iterable<String> getMediaContext(HttpServletRequest httpRequest) {
+		String encodedMediaCtx = this.getLocalMediaWebUrlContextParam(httpRequest);
+		encodedMediaCtx = encodedMediaCtx.replaceAll("\\?.*","");
+		return this.isLegacyPrettyUrlSupport() ? this.createLegacyLocalMediaWebUrlContext(httpRequest) : this.createLocalMediawebUrlContext(encodedMediaCtx);
+	}
 }
