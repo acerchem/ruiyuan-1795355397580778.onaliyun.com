002c8457e107898b16caa299550f3a90ca8db9c1
ricky.li <281423889@qq.com>
2019-06-17 11:32:02 +0800

    添加过滤

--- acerchem/acerchemfacades/src/com/acerchem/facades/storefinder/populators/SearchResultProductPricePopulator.java
+++ acerchem/acerchemfacades/src/com/acerchem/facades/storefinder/populators/SearchResultProductPricePopulator.java
@@ -1,5 +1,6 @@
 package com.acerchem.facades.storefinder.populators;
 
+import de.hybris.platform.commercefacades.product.data.ImageData;
 import de.hybris.platform.commercefacades.product.data.PriceData;
 import de.hybris.platform.commercefacades.product.data.PriceDataType;
 import de.hybris.platform.commercefacades.product.data.ProductData;
@@ -12,9 +13,12 @@ import de.hybris.platform.jalo.order.price.PriceInformation;
 import de.hybris.platform.product.ProductService;
 import de.hybris.platform.servicelayer.user.UserService;
 import java.math.BigDecimal;
+import java.util.List;
 import javax.annotation.Resource;
 import org.apache.commons.collections.CollectionUtils;
 import com.acerchem.core.model.UserLevelModel;
+import org.springframework.util.Assert;
+
 
 /**
  * Converter implementation for {@link de.hybris.platform.commerceservices.search.resultdata.SearchResultValueData} as
@@ -32,13 +36,52 @@ public class SearchResultProductPricePopulator extends SearchResultVariantProduc
 	@Resource
 	private CommercePriceService commercePriceService;
 
+	public void superPopulate(final SearchResultValueData source, final ProductData target)
+	{
+		Assert.notNull(source, "Parameter source cannot be null.");
+		Assert.notNull(target, "Parameter target cannot be null.");
+		// Pull the values directly from the SearchResult object
+		target.setCode(this.<String> getValue(source, "code"));
+		target.setName(this.<String> getValue(source, "name"));
+		List<String> arrayList = this.<List> getValue(source, "manufacturerName");
+		if(arrayList!=null&&CollectionUtils.isNotEmpty(arrayList))
+		{
+			target.setManufacturer(arrayList.get(0));
+		}
+		target.setDescription(this.<String> getValue(source, "description"));
+		target.setSummary(this.<String> getValue(source, "summary"));
+		target.setAverageRating(this.<Double> getValue(source, "reviewAvgRating"));
+		target.setConfigurable(this.<Boolean> getValue(source, "configurable"));
+		target.setBaseProduct(this.<String> getValue(source, "baseProductCode"));
+
+		populatePrices(source, target);
+
+		// Populate product's classification features
+		getProductFeatureListPopulator().populate(getFeaturesList(source), target);
+
+		final List<ImageData> images = createImageData(source);
+		if (CollectionUtils.isNotEmpty(images))
+		{
+			target.setImages(images);
+		}
+
+		populateUrl(source, target);
+		populatePromotions(source, target);
+		populateStock(source, target);
+	}
+
 	@Override
 	public void populate(final SearchResultValueData source, final ProductData target)
 	{
-        super.populate(source, target);
-        
-        final ProductModel productModel = productService.getProductForCode((String)source.getValues().get("code"));
-        final UserModel user = userService.getCurrentUser();
+		superPopulate(source, target);
+		if (source.getValues() != null)
+		{
+			target.setMultidimensional((Boolean) source.getValues().get(MULTIDIMENSIONAL));
+			setPriceRange(source, target);
+			setFirstCategoryNameList(source, target);
+		}
+      final ProductModel productModel = productService.getProductForCode((String)source.getValues().get("code"));
+      final UserModel user = userService.getCurrentUser();
 		final boolean isAnonymousUser = userService.isAnonymousUser(user);
 		final Boolean displayPrice = productModel.isAnonymousDisplayPrice();
 		
