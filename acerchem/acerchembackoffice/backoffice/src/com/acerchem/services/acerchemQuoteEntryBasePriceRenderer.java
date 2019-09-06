e42f75937c7c1785a7dc0ce461a30e37ad094a96
ricky.li <281423889@qq.com>
2019-09-06 17:14:36 +0800

    backoffice配置

---
+++ acerchem/acerchembackoffice/backoffice/src/com/acerchem/services/acerchemQuoteEntryBasePriceRenderer.java
@@ -0,0 +1,34 @@
+package com.acerchem.services;
+
+import com.hybris.cockpitng.dataaccess.facades.type.DataType;
+import com.hybris.cockpitng.engine.WidgetInstanceManager;
+import com.hybris.cockpitng.widgets.common.AbstractWidgetComponentRenderer;
+import de.hybris.platform.core.model.order.QuoteEntryModel;
+import de.hybris.platform.core.model.product.ProductModel;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zul.Div;
+import org.zkoss.zul.Label;
+
+import java.math.BigDecimal;
+
+
+public class acerchemQuoteEntryBasePriceRenderer extends AbstractWidgetComponentRenderer<Component, Object, QuoteEntryModel>
+{
+	public void render(Component parent, Object configuration, QuoteEntryModel data, DataType dataType, WidgetInstanceManager widgetInstanceManager) {
+		Div contactTypePanel = new Div();
+		Double basePrice = data.getBasePrice();
+		ProductModel product = data.getProduct();
+		if(product!=null && basePrice!=null){
+			String netweight = product.getNetWeight();
+			BigDecimal netweightBig = new BigDecimal(netweight);
+			BigDecimal basePriceBig = new BigDecimal(basePrice);
+			basePriceBig = basePriceBig.divide(netweightBig);
+			basePriceBig = basePriceBig.setScale(2, BigDecimal.ROUND_HALF_UP);
+			basePrice=basePriceBig.doubleValue();
+		}
+		Label contactTypeLabel = new Label(basePrice.toString());
+		contactTypePanel.appendChild(contactTypeLabel);
+		parent.appendChild(contactTypePanel);
+		this.fireComponentRendered(parent, configuration, data);
+	}
+}
---
+++ acerchem/acerchembackoffice/backoffice/src/com/acerchem/services/acerchemQuoteEntryQuantityRenderer.java
@@ -0,0 +1,33 @@
+package com.acerchem.services;
+
+import com.hybris.cockpitng.dataaccess.facades.type.DataType;
+import com.hybris.cockpitng.engine.WidgetInstanceManager;
+import com.hybris.cockpitng.widgets.common.AbstractWidgetComponentRenderer;
+import de.hybris.platform.core.model.order.QuoteEntryModel;
+import de.hybris.platform.core.model.product.ProductModel;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zul.Div;
+import org.zkoss.zul.Label;
+
+import java.math.BigDecimal;
+
+
+public class acerchemQuoteEntryQuantityRenderer extends AbstractWidgetComponentRenderer<Component, Object, QuoteEntryModel>
+{
+	public void render(Component parent, Object configuration, QuoteEntryModel data, DataType dataType, WidgetInstanceManager widgetInstanceManager) {
+		Div contactTypePanel = new Div();
+		Long quantity = data.getQuantity();
+		ProductModel product = data.getProduct();
+		if(product!=null && quantity!=null){
+			String netweight = product.getNetWeight();
+			BigDecimal netweightBig = new BigDecimal(netweight);
+			BigDecimal quantityBig = new BigDecimal(quantity);
+			quantityBig = quantityBig.multiply(netweightBig);
+			quantity = quantityBig.longValue();
+		}
+		Label contactTypeLabel = new Label(quantity.toString());
+		contactTypePanel.appendChild(contactTypeLabel);
+		parent.appendChild(contactTypePanel);
+		this.fireComponentRendered(parent, configuration, data);
+	}
+}
